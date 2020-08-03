62d4db67cba01de0cf7aa7819323de967fe47fc3
wmgriffi3 <48363930+wmgriffi3@users.noreply.github.com>
2020-08-03 14:54:52 -0700

    Update back_end_coding.py

--- back_end_coding.py
+++ back_end_coding.py
@@ -13,3 +13,17 @@ exercise_data = pd.read_csv(exercise_url, error_bad_lines=False)
 body_data.head()
 standards_data.head()
 exercise_data.head()
+
+
+
+from io import BytesIO
+
+import requests
+r = requests.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vQmHR9YOt6BhTolpVXR9m8eVSHM-2AnPmhVhYypo2liHRpZ1REQeEtE5rEoBQQfQfCt-5MGk6bU1ipQ/pub?output=csv')
+data = r.content
+
+dataf = pd.read_csv(BytesIO(data), index_col=0)
+
+dataf.head()
+
+
